5b2d771e7b18f1f9e655c08a0d1ba5373f30dac8
adi19947 <dano93@gmail.com>
2022-08-21 18:55:55 +0300

    adding backtick

--- stop-watch/step-01/stop-watch.js
+++ stop-watch/step-01/stop-watch.js
@@ -1,22 +1,13 @@
-console.log('hello');
+const stopWatchView = document.querySelector(' #stop-watch-view');
+let hundredth = 44;
+let seconds = 34;
+let minutes = 12;
 
-const stopWatchView = document.querySelector('#stop-watch-view');
-console.log(stopWatchView);
+function displayTime() {
+    //  stopWatchView.innerHTML = minutes + ':' + seconds + ':' + hundredth; 
 
-const pTag = document.querySelector(' p');
-console.log(pTag);
-const className = document.querySelector(' .my-class');
-console.log('class', className);
+    stopWatchView.innerHTML = `${minutes}:${seconds}:${hundredth}`;
+}
 
-const allParagraphs = document.querySelectorAll('p');
-console.log(allParagraphs);
-
-
-const listItems = document.querySelectorAll(' .list-item');
-console.log(listItems);
-let counter = 1;
-listItems.forEach(function (listItem) {
-    console.log('list item', listItem);
-    listItem.innerHTML = counter++;
-});
+displayTime();
 
